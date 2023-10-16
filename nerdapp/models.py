7ce6e2d2e62f892dcb542d469d9a361fdd39bca4
Manuel <manubarrerazurita@gmail.com>
2023-10-16 20:04:40 -0300

    Login done

--- nerdapp/models.py
+++ nerdapp/models.py
@@ -45,28 +45,24 @@ class Rol(models.Model):
 
     def __str__(self):
         return self.nombre_rol
-    
 class Usuario(models.Model):
-    id_usuario = models.IntegerField(primary_key=True, default=100)
-    rut = models.IntegerField(max_length=200)
-    nombre = models.CharField(max_length=200)
-    apellido_paterno = models.CharField(max_length=200)
-    apellido_materno = models.CharField(max_length=200)
-    telefono = models.IntegerField(max_length=200)
-    correo = models.EmailField()
-    direccion = models.CharField(max_length=200)
-    rol_id_rol = models.ForeignKey(Rol, null=False,  blank=False, on_delete=models.CASCADE)
-    comuna = models.ForeignKey(Comuna, null=False,  blank=False, on_delete=models.CASCADE)
+    id_usuario = models.IntegerField(primary_key=True)
+    nombre_usuario = models.CharField(max_length=200)
+    nombre = models.CharField(max_length=200, null=True)
+    apellido_paterno = models.CharField(max_length=200, null=True)
+    apellido_materno = models.CharField(max_length=200, null=True)
+    correo = models.CharField(max_length=200)
+    telefono = models.CharField(max_length=15)
 
     def __str__(self):
-        return f"{self.nombre} {self.apellido_paterno} {self.apellido_materno}"
+        return self.nombre_usuario
     
 class Subasta(models.Model):
-    id_subasta = models.IntegerField(primary_key=True, default=100)
+    id_subasta = models.IntegerField(primary_key=True)
     nombre = models.CharField(max_length=200)
     descripcion = models.CharField(max_length=200)
-    precio_inicial = models.IntegerField(max_length=200)
-    precio_mas_alto = models.IntegerField(max_length=200)
+    precio_inicial = models.IntegerField()
+    precio_mas_alto = models.IntegerField()
     fecha_inicio = models.DateField()
     fecha_termino = models.DateField()
     hora_inicio = models.TimeField()
@@ -76,34 +72,34 @@ class Subasta(models.Model):
         return self.nombre
     
 class Comentario(models.Model):
-    id_comentario = models.IntegerField(primary_key=True, default=100)
+    id_comentario = models.IntegerField(primary_key=True)
     comentario = models.CharField(max_length=200)
     fecha_comentario = models.DateField()
     estado_comentario = models.CharField(max_length=200)
     usuario_id_usuario = models.ForeignKey(Usuario, null=False, blank=False, on_delete=models.CASCADE)
 
 class Carrito(models.Model):
-    id_carrito = models.IntegerField(primary_key=True, default=100)
+    id_carrito = models.IntegerField(primary_key=True)
     fecha_compra = models.DateField()
-    total_venta = models.IntegerField(max_length=200)
-    iva = models.IntegerField(max_length=200)
+    total_venta = models.IntegerField()
+    iva = models.IntegerField()
     usuario_id_usuario = models.ForeignKey(Usuario, null=False, blank=False, on_delete=models.CASCADE)
 
 class Usuario_subasta(models.Model):
-    id_usuario_subasta = models.IntegerField(primary_key=True, default=100)
+    id_usuario_subasta = models.IntegerField(primary_key=True)
     usuario_id_usuario = models.ForeignKey(Usuario, null=False, blank=False, on_delete=models.CASCADE)
     subasta_id_subasta = models.ForeignKey(Subasta, null=False, blank=False, on_delete=models.CASCADE)
 
 class Publicacion(models.Model):
-    id_publicacion = models.IntegerField(primary_key=True, default=100)
+    id_publicacion = models.IntegerField(primary_key=True)
     titulo_publicacion = models.CharField(max_length=200)
-    descripcion_publicacion = models.IntegerField(max_length=200) 
+    descripcion_publicacion = models.IntegerField() 
     fecha_publicacion = models.DateField()
     estado_publicacion = models.CharField(max_length=200)
     usuario_id_usuario = models.ForeignKey(Usuario, null=False, blank=False, on_delete=models.CASCADE)
 
 class Foto(models.Model):
-    id_foto = models.IntegerField(primary_key=True, default=100)
+    id_foto = models.IntegerField(primary_key=True)
     foto = models.CharField(max_length=200)
     subasta_id_subasta = models.ForeignKey(Subasta, null=False, blank=False, on_delete=models.CASCADE)
     producto_id_producto = models.ForeignKey(Producto, null=False, blank=False, on_delete=models.CASCADE)
--- nerdapp/views.py
+++ nerdapp/views.py
@@ -1,22 +1,20 @@
 from django.shortcuts import render
-from .models import Categoria  # Asegúrate de importar tu modelo Producto
-
-# Create your views here.
+from .models import Categoria
+from django.contrib.auth.views import LoginView
 
 def hola_mundo(request):
     return render(request, 'nerdapp/index.html')
 
+def dashboard(request):
+    return render(request, 'nerdapp/dashboard.html')
+
 def page2(request):
     return render(request, 'nerdapp/page2.html')
 
-def login(request):
-    return render(request, 'nerdapp/login.html')
-
-#def lista_categorias(request):
-#   categorias = Categoria.objects.all()  # Consultar todos los productos de la base de datos
-#   return render(request, 'lista_categorias.html', {'categorias': categorias})
-
 def lista_categorias(request):
     categorias = Categoria.objects.all()
-    print(categorias)  # Muestra las categorías en la consola
+    print(categorias)
     return render(request, 'nerdapp/lista_categorias.html', {'categorias': categorias})
+
+class CustomLoginView(LoginView):
+    template_name = 'nerdapp/login.html'
